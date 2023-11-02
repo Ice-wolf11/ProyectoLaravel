2098eb8314249513ef77c3bc4d1fd648b4ef3c1b
Andrew <andrewamedinaw@gmail.com>
2023-11-01 22:19:52 -0500

    creando las migraciones, controladores y modelos

---
+++ app/Models/tipoDeDocumento.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class tipoDeDocumento extends Model
+{
+    use HasFactory;
+}
